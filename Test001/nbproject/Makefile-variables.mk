73bf7649092020bca314f6568c6ce098ce8cbd50
dvptm <dvptm@solaris>
2020-04-21 00:06:36 +0900

    First

---
+++ Test001/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=OracleDeveloperStudio-Solaris-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/OracleDeveloperStudio-Solaris-x86
+CND_ARTIFACT_NAME_Debug=libtest001.a
+CND_ARTIFACT_PATH_Debug=dist/Debug/OracleDeveloperStudio-Solaris-x86/libtest001.a
+CND_PACKAGE_DIR_Debug=dist/Debug/OracleDeveloperStudio-Solaris-x86/package
+CND_PACKAGE_NAME_Debug=Test001.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/OracleDeveloperStudio-Solaris-x86/package/Test001.tar
+# Release configuration
+CND_PLATFORM_Release=OracleDeveloperStudio-Solaris-x86
+CND_ARTIFACT_DIR_Release=dist/Release/OracleDeveloperStudio-Solaris-x86
+CND_ARTIFACT_NAME_Release=libtest001.a
+CND_ARTIFACT_PATH_Release=dist/Release/OracleDeveloperStudio-Solaris-x86/libtest001.a
+CND_PACKAGE_DIR_Release=dist/Release/OracleDeveloperStudio-Solaris-x86/package
+CND_PACKAGE_NAME_Release=Test001.tar
+CND_PACKAGE_PATH_Release=dist/Release/OracleDeveloperStudio-Solaris-x86/package/Test001.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
